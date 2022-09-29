1760f5d71d89373951a6c21f841b192a2ca04f8e
sandeep majithiya <sandip.jnext@gmail.com>
2022-09-29 16:18:37 +0530

    my bash trigger

--- wordpr.sh
+++ wordpr.sh
@@ -8,7 +8,8 @@ sudo apt install apache2 -y
 sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo apt install mysql-server -y
-sudo apt install ghostscript \
+sudo apt install curl \
+                 ghostscript \
                  libapache2-mod-php \
                  php \
                  php-bcmath \
@@ -21,13 +22,16 @@ sudo apt install ghostscript \
                  php-xml  \
 				 php-zip   -y
              
+suco chmod 777 /var/www/https
+suco chmod 777 /var/www
 
 sudo yum install php-gd -y 
 sudo systemctl start mariadb
+sudo systemctl enable mariadb
 sudo mkdir -p /srv/www
  sudo chown www-data: /srv/www
  curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar -xzf -C /srv/www
- git clone https://github.com/sandeep-sk512/MasterDev.git
+ #git clone https://github.com/sandeep-sk512/MasterDev.git
 cp MasterDev/wordpress.conf  /etc/apache2/sites-available/wordpress.conf
 
    sudo a2ensite wordpress
@@ -35,15 +39,14 @@ cp MasterDev/wordpress.conf  /etc/apache2/sites-available/wordpress.conf
    sudo a2dissite 000-default
    sudo service apache2 reload
 
-   mysql -u root -e 'CREATE DATABASE wordpress;'
-   mysql -u root -e 'CREATE USER "wordpress"@"localhost" IDENTIFIED BY "admin123"; '
-   mysql -u root -e 'GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wordpress@localhost IDENTIFIED BY "admin123";'
-   mysql -u root -e 'FLUSH PRIVILEGES;'
-   mysql -u root -e 'quit'
+   mysql -u root -e "CREATE DATABASE wordpress;"
+   mysql -u root -e "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'admin123'; "
+   mysql -u root -e "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'admin123';"
+   mysql -u root -e "FLUSH PRIVILEGES;"
+   mysql -u root -e "quit"
    
    sudo -u www-data cp /srv/www/wordpress/wp-config-sample.php /srv/www/wordpress/wp-config.php
    sudo -u www-data sed -i 's/database_name_here/wordpress/' /srv/www/wordpress/wp-config.php
    sudo -u www-data sed -i 's/username_here/wordpress/' /srv/www/wordpress/wp-config.php
    sudo -u www-data sed -i 's/password_here/admin123/' /srv/www/wordpress/wp-config.php
    service mysql restart
-sudo systemctl enable httpd && sudo systemctl enable mariadb
